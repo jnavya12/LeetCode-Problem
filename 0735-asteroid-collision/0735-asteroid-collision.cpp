70900e3c8a013c58974722a6e4ba0166a8508d9f
Navya Jain <125814034+jnavya12@users.noreply.github.com>
2025-03-09 12:55:52 +0530

    Time: 3 ms (29.73%), Space: 21.6 MB (79.47%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> stack;
+        for (int ast : asteroids) {
+            bool destroyed = false;
+            while (!stack.empty() && ast < 0 && stack.back() > 0) {
+                if (stack.back() < -ast) {
+                    stack.pop_back();
+                    continue;
+                } else if (stack.back() == -ast) {
+                    stack.pop_back();
+                }
+                destroyed = true;
+                break;
+            }
+            if (!destroyed) {
+                stack.push_back(ast);
+            }
+        }
+        return stack;
+    }
+};
